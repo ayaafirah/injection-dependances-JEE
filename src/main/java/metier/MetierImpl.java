30391c4d3b1057c99ffab095963ae91d74751fb7
Aya <ayaafirah@gmail.com>
2024-03-05 22:32:19 +0100

    3nd Commit

--- src/main/java/metier/MetierImpl.java
+++ src/main/java/metier/MetierImpl.java
@@ -1,8 +1,7 @@
 package metier;
 
 import dao.IDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.stereotype.Component;
 
 @Component
